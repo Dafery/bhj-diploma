b7bdeda8984b427f6e8e708248a318937b68161b
Maria Abramovich <mashkala2@gmail.com>
2022-12-29 01:02:45 +0300

    changed: public/js/api, .gitignore

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,35 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  let { url, data, method, callback } = options;
 
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(null, xhr.response);
+    }
+  };
+
+  try {
+    if (method === 'GET') {
+      url += '?';
+      for (let key in data) {
+        url += `${key}=${data[key]}&`;
+      }
+
+      xhr.open('GET', url.slice(0, -1));
+      xhr.send();
+    } else {
+      const formData = new FormData();
+      for (let key in data) {
+        formData.append(key, data[key]);
+      }
+
+      xhr.open(method, url);
+      xhr.send(formData);
+    }
+  } catch (error) {
+    callback(error);
+  }
 };
